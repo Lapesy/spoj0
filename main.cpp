5d20f3a85b82a177a4a716b17f1e5edd68e7b024
Michał Nowak <eniek132@gmail.com>
2018-12-04 12:26:03 +0100

    nowe zadanie

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+
+    cin>>a;
+
+    string b[a];
+
+    for(int f=0;f<a;f++)
+    {
+        cin>>b[f];
+    }
+
+    char c=b[0].at(0);
+    cout << c;
+    unsigned int amountc=0;
+    for(int i=0;i<b[0].length();i++)
+    {
+        if(b[0].at(i)==c)
+        {
+            amountc+=1;
+        }
+    }
+    cout << amountc;
     return 0;
 }
